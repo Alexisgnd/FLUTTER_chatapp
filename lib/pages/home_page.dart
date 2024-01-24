a0969ecfb6be7410bd095dca1cb7e8802b6385e7
Alexis GONNEAUD <alexis.gonneaud@hotmail.fr>
2024-01-24 01:38:28 +0100

    Add : added send & receive messages fonctionnality

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,8 +15,11 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
       appBar: AppBar(
         title: const Text("Home"),
+        backgroundColor: Colors.transparent,
+        foregroundColor: Colors.grey,
       ),
       drawer: const MyDrawer(),
       body: _buildUserList()
@@ -58,6 +61,7 @@ class HomePage extends StatelessWidget {
             MaterialPageRoute(
               builder: (context) => ChatPage(
                 receiverEmail: userData["email"],
+                receiverID: userData["uid"],
               ),
               ),
           );
